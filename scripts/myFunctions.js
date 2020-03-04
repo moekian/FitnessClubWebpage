32626fba41c6a3b72c15d0c7a3c315ac22632b51
Mohammad Kiani <kianimohammad@outlook.com>
2020-03-04 12:16:47 -0500

    add js to project

---
+++ scripts/myFunctions.js
@@ -0,0 +1,24 @@
+
+
+//Thank you alert
+function tyAlert() {
+	alert("Thank you for your interest in our fitness club. We will contact you soon.");
+}
+
+//Burpees video window
+function burpees() {
+	window.open("scripts/burpees.html", "_blank", "width=610, height=360");
+	document.getElementById('video').play();
+}
+
+//Plank video window
+function plank() {
+	window.open("scripts/plank.html", "_blank", "width=610, height=360");
+	document.getElementById('video').play();
+}
+
+//Mountain climber video window
+function mtnClimber() {
+	window.open("scripts/mtn.html", "_blank", "width=610, height=360");
+	document.getElementById('video').play();
+}
